5ce9c70da3033f5e0397093dd4ec73dfd60c19cf
bthaob8a38-boop <bthaob8a38@gmail.com>
2026-07-05 21:16:39 +0800

    Add batch experiment runner skeleton

---
+++ scripts/run_all_experiments.m
@@ -0,0 +1,14 @@
+% FRDR Project - Run All Experiments
+% This is a skeleton script for batch experiments
+
+clear; clc;
+
+fprintf('Starting FRDR batch experiments...\n');
+
+% TODO: load datasets
+% TODO: run FRDR algorithm
+% TODO: run baseline methods
+% TODO: evaluate performance
+% TODO: save results
+
+fprintf('Experiments completed.\n');
