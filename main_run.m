99fe60a4a88a7b686fdec264363c750bc76712e1
bthaob8a38-boop <bthaob8a38@gmail.com>
2026-07-05 21:16:43 +0800

    Add main FRDR entry script skeleton

---
+++ main_run.m
@@ -0,0 +1,25 @@
+% FRDR Project - Main Entry
+% One-click run on UCI datasets (skeleton version)
+
+clear; clc;
+
+fprintf('=================================\n');
+fprintf(' FRDR Project Main Runner\n');
+fprintf('=================================\n');
+
+% TODO: select dataset
+dataset_name = 'UCI_sample';
+
+% TODO: load data
+fprintf('Loading dataset: %s\n', dataset_name);
+
+% TODO: run FRDR algorithm
+fprintf('Running FRDR...\n');
+
+% TODO: feature selection
+fprintf('Performing attribute reduction...\n');
+
+% TODO: evaluation
+fprintf('Evaluating results...\n');
+
+fprintf('Done.\n');
